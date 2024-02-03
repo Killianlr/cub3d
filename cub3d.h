830f4fd3b819b83cfe65bcfc83ff250738de23d3
flavian <flavian.serpe1@gmail.com>
2024-02-03 19:20:32 +0100

    includes

--- cub3d.h
+++ cub3d.h
@@ -1,6 +1,17 @@
-
 #ifndef CUB3D_H
 # define CUB3D_H
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <math.h>
+# include "minilibx-linux/mlx.h"
+# include <X11/X.h>
+# include <X11/keysym.h>
 
 int main(void);
 
